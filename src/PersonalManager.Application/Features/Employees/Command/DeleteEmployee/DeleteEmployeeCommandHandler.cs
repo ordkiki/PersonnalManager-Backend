2e46af88f70acb20ce4acaf14a757134cda953b8
ordkiki <onjaniaina235497@gmail.com>
2025-10-15 22:00:26 +0300

    Delete employee

---
+++ src/PersonalManager.Application/Features/Employees/Command/DeleteEmployee/DeleteEmployeeCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using PersonaManager.Domain.Entities;
+using PersonaManager.Domain.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalManager.Application.Features.Employees.Command.DeleteEmployee
+{
+    public class DeleteEmployeeCommandHandler(IRepositoryCommand<Employee> _repo, IUnitOfWork _unit) : IRequestHandler<DeleteEmployeeCommand, bool>
+    {
+        public async Task<bool> Handle(DeleteEmployeeCommand request, CancellationToken cancellationToken)
+        {
+            bool result = await _repo.DeleteAsync(request.Id);
+            await _unit.SaveChangeAsync(cancellationToken);
+            return result;
+        }
+    }
+}
