0c28855f634484621faf4f7e2725ad9f8d3b3fab
ordkiki <onjaniaina235497@gmail.com>
2025-10-15 21:51:13 +0300

    mise a jours de l'education

---
+++ src/PersonalManager.Application/Features/Educations/Command/UpdateEducation/UpdateEducationCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using PersonalManager.Application.Features.Educations.Command.UpdateEmployeeEducation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalManager.Application.Features.Educations.Command.UpdateEducation
+{
+    public class UpdateEducationCommand : IRequest<UpdateEducationResponse>
+    {
+        public Guid? Id { get; set; }
+        public string? Graduation { get; set; }
+        public string? FieldOfStudy { get; set; }
+        public string? Establishment { get; set; }
+        public DateTime? GraduationYear { get; set; }
+    }
+}
---
+++ src/PersonalManager.Application/Features/Educations/Command/UpdateEducation/UpdateEducationResponse.cs
@@ -0,0 +1,23 @@
+﻿using PersonalManager.Application.Features.Employees.Command.CreateEmployeeEducation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalManager.Application.Features.Educations.Command.UpdateEmployeeEducation
+{
+    
+    public record UpdateEducation
+    {
+        public string? Graduation { get; set; }
+        public string? FieldOfStudy { get; set; }
+        public string? Establishment { get; set; }
+        public DateTime? GraduationYear { get; set; }
+    }
+    public class UpdateEducationResponse
+    {
+        public Guid? Id { get; set; }
+        public UpdateEducation? Education { get; set; }
+    }
+}
