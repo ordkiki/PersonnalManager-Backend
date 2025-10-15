eccee4f58c1f56a902019fc7adf9f05a0346b4ea
ordkiki <onjaniaina235497@gmail.com>
2025-10-15 22:15:04 +0300

    update EmployeStatus

---
+++ src/PersonalManager.Application/Features/Employees/Command/UpdateEmployeeStatus/UpdateEmployeeStatusCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using PersonaManager.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalManager.Application.Features.Employees.Command.UpdateEmployeeStatus
+{
+    public record UpdateEmployeeStatusCommand : IRequest<UpdateEmployeeStatusResponse>
+    {
+        public required Guid EmployeeId { get; init; }
+        public EmployeeStatus Status { get; set; }
+    }
+}
---
+++ src/PersonalManager.Application/Features/Employees/Command/UpdateEmployeeStatus/UpdateEmployeeStatusResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalManager.Application.Features.Employees.Command.UpdateEmployeeStatus
+{
+    public class UpdateEmployeeStatusResponse
+    {
+        public required Guid EmployeeId { get; set; }
+        public string Status { get; set; }
+    }
+}
