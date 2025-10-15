293932caf08d2dae9ad8af00bbf0442fecfa2f9e
ordkiki <onjaniaina235497@gmail.com>
2025-10-15 21:36:54 +0300

    delete education

---
+++ src/PersonalManager.Application/Features/Educations/Command/DeleteEducation/DeleteEducationCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using PersonaManager.Domain.Entities;
+using PersonaManager.Domain.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalManager.Application.Features.Educations.Command.DeleteEducation
+{
+    public class DeleteEducationCommandHandler(IRepositoryCommand<Education> _repo) : IRequestHandler<DeleteEducationCommand, bool>
+    {
+        public async Task<bool> Handle(DeleteEducationCommand request, CancellationToken cancellationToken)
+        {
+            return await _repo.DeleteAsync(request.Id, cancellationToken);
+        }
+    }
+}
