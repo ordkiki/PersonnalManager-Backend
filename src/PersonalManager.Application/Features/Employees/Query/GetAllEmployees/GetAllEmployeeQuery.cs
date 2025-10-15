faef77ad007babdcb29a1b5d2f679cde15a4d74b
ordkiki <onjaniaina235497@gmail.com>
2025-10-15 23:01:56 +0300

    GetAll, v1.0

---
+++ src/PersonalManager.Application/Features/Employees/Query/GetAllEmployees/GetAllEmployeeQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalManager.Application.Features.Employees.Query.GetAllEmployees
+{
+    public class GetAllEmployeeQuery : IRequest<GetAllEmployeeQueryResponse>
+    {
+        public string? Search { get; set; }
+        public int? Limit { get; set; }
+        public int? Page { get; set; }
+        public string? OrderBy { get; set; }
+        public bool? SortazAZ { get; set; }
+    }
+}
---
+++ src/PersonalManager.Application/Features/Employees/Query/GetAllEmployees/GetAllEmployeeQueryResponse.cs
@@ -0,0 +1,16 @@
+﻿using PersonaManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalManager.Application.Features.Employees.Query.GetAllEmployees
+{
+    public class GetAllEmployeeQueryResponse
+    {
+        public IEnumerable<Employee>? Data { get; set; }
+        public long? Total { get; set; }
+        public int? TotalPage { get; set; }
+    }
+}
