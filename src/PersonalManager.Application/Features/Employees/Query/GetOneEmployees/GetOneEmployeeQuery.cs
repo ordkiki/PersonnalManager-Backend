358738dac3abd9e59788d27b26ccec7763d65607
ordkiki <onjaniaina235497@gmail.com>
2025-10-17 11:56:46 +0300

    getOne

---
+++ src/PersonalManager.Application/Features/Employees/Query/GetOneEmployees/GetOneEmployeeQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalManager.Application.Features.Employees.Query.GetOneEmployees
+{
+    public class GetOneEmployeeQuery : IRequest<GetOneEmployeeResponse>
+    {
+        public string? Term { get; set; }        
+    }
+}
